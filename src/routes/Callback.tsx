c4dfd8b9ef4bc28b605c2314c4a3866cde2b299a
横田昴輝 <kokiyokotakoki@gmail.com>
2023-09-02 13:27:00 +0900

    useCallbackの例 - (非useCallback状態)

--- src/routes/Callback.tsx
+++ src/routes/Callback.tsx
@@ -1,14 +1,38 @@
-import React, {useCallback} from 'react'
+import React, { useState, useCallback, useEffect } from "react";
 
-const Callback  = () => {
-    const sample1 = useCallback((value: number) => {
-        console.log(value);
-    }, [])
-    return (
-        <>
-            <div>callback</div>
-        </>
-    )
+// ① toggleが切り替わるたびにレンダリングが走り、関係ないChildコンポーネントも再レンダリングされる。
+
+const Callback = () => {
+  const [count, setCount] = useState(0);
+  const [toggle, setToggle] = useState(false);
+
+  const multiplication = () => {
+    return count * count;
+  };
+
+  return (
+    <div className="App">
+      {count}
+      <input
+        type="number"
+        value={count}
+        onChange={(e) => setCount(parseInt(e.target.value))}
+      />
+      <button onClick={() => setToggle(!toggle)}> toggle </button>
+      <Child multiplication={multiplication} />
+    </div>
+  );
 }
 
-export default Callback
+const Child = ({ multiplication }) => {
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    console.log('Child');
+    setValue(multiplication);
+  })
+
+  return <p>{value}</p>
+}
+
+export default Callback;
